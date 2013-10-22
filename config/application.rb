e199a9ba6bde4b01130df6c97a56bc9c8d4c9605
Rob McKinnon <@digital.justice.gov.uk>
2013-10-22 11:51:48 +0100

    load mongoid.yml

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,11 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'boot'
 
+puts "==== env: #{ENV['RACK_ENV']}"
+
 Bundler.require :default, ENV['RACK_ENV']
+
+Mongoid.load!( File.dirname(__FILE__) + "/../config/mongoid.yml")
 
 Dir[File.expand_path('../../api/*.rb', __FILE__)].each do |f|
   require f
