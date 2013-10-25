cff00aaeeadf23567a103539a8447888e2bf8b0a
Rob McKinnon <@digital.justice.gov.uk>
2013-10-25 16:14:28 +0100

    address_lines -> address_line1, address_line2, address_line3

--- app/models/address.rb
+++ app/models/address.rb
@@ -13,7 +13,7 @@ class Address
   embedded_in :addressable, polymorphic: true
 
   entity do
-    [ :address_lines, :town, :county, :postcode, :country ].each do |field|
+    [ :address_lines1, :address_lines2, :address_lines3, :town, :county, :postcode, :country ].each do |field|
       expose field, :if => lambda { |object, options| object.send(field) }
     end
   end
