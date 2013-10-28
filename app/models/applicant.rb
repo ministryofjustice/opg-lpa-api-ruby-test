b30272e86d60c7dce23114c96b7d400c4b85328d
Rob McKinnon <@digital.justice.gov.uk>
2013-10-28 14:44:33 +0000

    added applicant to LPA

---
+++ app/models/applicant.rb
@@ -0,0 +1,16 @@
+require_relative 'person_name'
+
+class Applicant
+  include Mongoid::Document
+  include Grape::Entity::DSL
+
+  include PersonName
+
+  embeds_one :address, as: :addressable
+
+  entity do
+    expose :_id
+
+    expose :address, :using => Address::Entity, :if => lambda { |object, options| object.address }
+  end
+end
