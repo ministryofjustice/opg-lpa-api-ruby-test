dd05531eb17df77b1fe73b715a4e5486bafb6fe8
James Darling <james@abscond.org>
2013-10-30 13:22:37 +0000

    Add support for storing when_to_use on LPA

--- app/models/lpa.rb
+++ app/models/lpa.rb
@@ -5,6 +5,7 @@ class Lpa
   include Grape::Entity::DSL
 
   field :type, type: String
+  field :when_to_use, type: String
 
   belongs_to :applicant
 
@@ -13,12 +14,14 @@ class Lpa
   embeds_many :attorneys
 
   validates :type, presence: false, length: { minimum: 2, allow_blank: true }
+  validates :when_to_use, presence: false, length: { minimum: 2, allow_blank: true }
   validates :applicant, presence: true
 
   include Identifier
 
   entity do
     expose :type, if: lambda { |object, options| object.type }
+    expose :when_to_use, if: lambda { |object, options| object.type }
 
     expose :applicant, using: Applicant::Entity, if: lambda { |object, options| object.applicant }
     expose :donor,     using: Donor::Entity,     if: lambda { |object, options| object.donor }
