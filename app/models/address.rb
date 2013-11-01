4fdcbefe4e0f826e521b8a89d12c5fbe5d9242d7
James Darling <james@abscond.org>
2013-11-01 16:18:15 +0000

    fix typo in address lines exposing

--- app/models/address.rb
+++ app/models/address.rb
@@ -7,13 +7,13 @@ class Address
   field :address_line3, type: String
   field :town,          type: String
   field :county,        type: String
-  field :post_code,      type: String
+  field :post_code,     type: String
   field :country,       type: String
 
   embedded_in :addressable, polymorphic: true
 
   entity do
-    [ :address_lines1, :address_lines2, :address_lines3, :town, :county, :post_code, :country ].each do |field|
+    [ :address_line1, :address_line2, :address_line3, :town, :county, :post_code, :country ].each do |field|
       expose field, :if => lambda { |object, options| object.send(field) }
     end
   end
