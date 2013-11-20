79caf080efd34ca99360eeb52cb226b395ef674e
Rob McKinnon <@digital.justice.gov.uk>
2013-11-20 11:21:06 +0000

    refactoring variable name

--- api/post_helpers.rb
+++ api/post_helpers.rb
@@ -2,21 +2,20 @@ module Opg
   module PostHelpers
 
     def handle_post
-      attributes = params.except('route_info','format')
-      def attributes.permitted?; true; end
+      attributes = params.except('route_info','format').to_hash
 
       begin
         host = [ request.env['rack.url_scheme'], '://', request.host ].join('')
 
-        object = yield attributes
+        resource = yield attributes
 
-        if object.persisted?
-          uri = [ host, request.path, '/', object.id, '.json' ].join('')
-          object.update_attribute(:uri, uri)
+        if resource.persisted?
+          uri = [ host, request.path, '/', resource.id, '.json' ].join('')
+          resource.update_attribute(:uri, uri)
 
-          present object, with: object.class.const_get(:Entity)
+          present resource, with: resource.class.const_get(:Entity)
         else
-          unprocessable_entity_error! error_messages(object)
+          unprocessable_entity_error! error_messages(resource)
         end
       rescue Mongoid::Errors::UnknownAttribute => e
         unprocessable_entity_error! unknown_attribute_message(e)
